59cbe0a39c0ddb73c21513a020ee0c12b752b1fa
victoravtr <victoravtr@gmail.com>
2023-12-30 13:57:03 +0100

    Added solution for adventjs Day 10

---
+++ adventjs.dev/Day10/solution.js
@@ -0,0 +1,21 @@
+function createChristmasTree(ornaments, height) { // FIXME: I think  I can do it a bit better but don't have time now
+	let result = "";
+	const spaces = " ".repeat(height - 1);
+
+	const ornamentsArray = [...ornaments.repeat(height)].join(" ");
+	let currentPosition = 0;
+
+	for (const index of new Array(height).keys()) {
+		const length = index + 1;
+		const total = currentPosition + length;
+
+		result +=
+			spaces.slice(index) +
+			ornamentsArray.slice(currentPosition * 2, total * 2 - 1) +
+			"\n";
+
+		currentPosition = total % ornaments.length;
+	}
+
+	return result + spaces + "|\n";
+}
